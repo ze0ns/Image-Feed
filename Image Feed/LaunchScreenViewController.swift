69968b022c146447e464d27aa41568186714905b
zeons <oao@i-cio.ru>
2026-01-15 21:44:51 +0300

    Add TableView

--- Image Feed/LaunchScreenViewController.swift
+++ Image Feed/LaunchScreenViewController.swift
@@ -8,10 +8,60 @@
 
 import UIKit
 
-class LaunchScreenViewController: UIViewController {
-
+class ImagesListViewController: UIViewController {
+    let tableView = UITableView()
+    var characters = ["Link", "Zelda", "Ganondorf", "Midna"]
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupTableView()
+        
+        
     }
+    func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+      }
+}
+extension ImagesListViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return characters.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = characters[indexPath.row]
+    return cell
+  }
+}
+
+//MARK: - SwiftUI
+import SwiftUI
+struct ImagesListVCProvider: PreviewProvider{
+    
+    static var previews: some View{
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let tabBarVC = ImagesListViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ImagesListVCProvider.ContainerView>) ->
+        ImagesListViewController{
+            return tabBarVC
+        }
+        func updateUIViewController(_ uiViewController: ImagesListVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ImagesListVCProvider.ContainerView>) {
+            
+        }
+    }
+    
 }
