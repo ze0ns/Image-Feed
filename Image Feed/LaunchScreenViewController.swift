6eb4c89cc034400f54123ad7bdad86f89fab3a3b
zeons <oao@i-cio.ru>
2026-01-15 13:02:40 +0300

    LaunchScreen

---
+++ Image Feed/LaunchScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenViewController.swift
+//  Image Feed
+//
+//  Created by Oschepkov Aleksandr on 15.01.2026.
+//
+
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
