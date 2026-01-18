62daed99af2d323fa2795e86c5581b1bb1a3c0da
zeons <oao@i-cio.ru>
2026-01-18 09:27:41 +0300

    refactor: Удаление кода - Удален лишний код SwiftUI

--- Image Feed/ImagesListCell/LaunchScreenViewController.swift
+++ Image Feed/ImagesListCell/LaunchScreenViewController.swift
@@ -73,26 +73,3 @@ extension ImagesListViewController: UITableViewDelegate{
     }
 }
 
-//
-//MARK: - SwiftUI
-import SwiftUI
-struct ImagesListVCProvider: PreviewProvider{
-    
-    static var previews: some View{
-        ContainerView().edgesIgnoringSafeArea(.all)
-    }
-    
-    struct ContainerView: UIViewControllerRepresentable {
-        
-        let tabBarVC = ImagesListViewController()
-        
-        func makeUIViewController(context: UIViewControllerRepresentableContext<ImagesListVCProvider.ContainerView>) ->
-        ImagesListViewController{
-            return tabBarVC
-        }
-        func updateUIViewController(_ uiViewController: ImagesListVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ImagesListVCProvider.ContainerView>) {
-            
-        }
-    }
-    
-}
