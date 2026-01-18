ca723d33b2d307d063ef2fc9350e021b563a5897
zeons <oao@i-cio.ru>
2026-01-18 21:16:12 +0300

    refactor: Изменение ресурсов у названий цветов

--- Image Feed/Helpers/UIColor+Extensions.swift
+++ Image Feed/Helpers/UIColor+Extensions.swift
@@ -1,9 +1,9 @@
 import UIKit
 extension UIColor {
-    static var ypGreen: UIColor { UIColor(named: "YP_Green") ?? UIColor.green }
-    static var ypRed: UIColor { UIColor(named: "YP_Red") ?? UIColor.red }
-    static var ypBlack: UIColor { UIColor(named: "YP_Black") ?? UIColor.black}
-    static var ypBackground: UIColor { UIColor(named: "YP_Background") ?? UIColor.darkGray }
-    static var ypGray: UIColor { UIColor(named: "YP_Gray") ?? UIColor.gray }
-    static var ypWhite: UIColor { UIColor(named: "YP_White") ?? UIColor.white}
+    static var ypGreen: UIColor { UIColor(resource: .ypGreen) }
+    static var ypRed: UIColor { UIColor(resource: .ypRed)  }
+    static var ypBlack: UIColor { UIColor(resource: .ypBlack) }
+    static var ypBackground: UIColor { UIColor(resource: .ypBackground) }
+    static var ypGray: UIColor { UIColor(resource: .ypGray)  }
+    static var ypWhite: UIColor { UIColor(resource: .ypWhite) }
 }
