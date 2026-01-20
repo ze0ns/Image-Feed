c1feec803ec89f130b9f0309e2b5956a45278bbd
zeons <oao@i-cio.ru>
2026-01-20 12:20:07 +0300

    feat: Обработка действий по нажатию - Добавлено открытие на большом экране фотографии на которую нажали

--- Image Feed/ImagesListCell/ImagesListViewController.swift
+++ Image Feed/ImagesListCell/ImagesListViewController.swift
@@ -13,6 +13,7 @@ final class ImagesListViewController: UIViewController {
     private let tableView = UITableView()
     private var imageLists =  ImagesListViewController.imagesListMock
     private let cellId = "ImagesListCell"
+    var imageURL = UIImage(resource: ._13)
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -27,7 +28,7 @@ final class ImagesListViewController: UIViewController {
         view.backgroundColor = .ypBlack
         setupTableView()
         configureTable()
-
+        
     }
     // MARK: - Private Methods
     private func setupTableView() {
@@ -45,12 +46,15 @@ final class ImagesListViewController: UIViewController {
             tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
-    ])
+        ])
     }
     private func changeRootToDestination() {
-              let navController = UINavigationController(rootViewController: SingleImageViewController())
-              present(navController, animated: true)
-        }
+        let fullScreenImageVC = SingleImageViewController()
+        fullScreenImageVC.imageURL = imageURL
+        let navController = UINavigationController(rootViewController: fullScreenImageVC)
+        navController.modalPresentationStyle = .fullScreen
+        present(navController, animated: true)
+    }
     
 }
 // MARK: - Extension TableViewCell 
@@ -63,18 +67,20 @@ extension ImagesListViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ImageCell
         let currentImage = imageLists[indexPath.row]
         let maxImageWidth = tableView.bounds.width - 16
-            cell.configure(
-                image: currentImage.image,
-                islike: UIImage(resource: currentImage.like ? .likeButtonOn : .likeButtonOff),
-                date: dateFormatter.string(from: Date()),
-                maxImageWidth: maxImageWidth
-            )
+        cell.configure(
+            image: currentImage.image,
+            islike: UIImage(resource: currentImage.like ? .likeButtonOn : .likeButtonOff),
+            date: dateFormatter.string(from: Date()),
+            maxImageWidth: maxImageWidth
+        )
         cell.selectionStyle = .none
         return cell
     }
 }
 extension ImagesListViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let currentImage = imageLists[indexPath.row]
+        imageURL = currentImage.image
         changeRootToDestination()
     }
 }
--- Image Feed/ImagesListCell/SingleImageViewController.swift
+++ Image Feed/ImagesListCell/SingleImageViewController.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class SingleImageViewController: UIViewController {
     private let imagesView = UIImageView()
-    
+    var imageURL = UIImage()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         setupConstraints()
     }
     private func setupViews() {
-        imagesView.image = UIImage(resource: ._10)
+        imagesView.image = imageURL
         imagesView.contentMode = .scaleAspectFill
         imagesView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(imagesView)
@@ -30,24 +30,22 @@ class SingleImageViewController: UIViewController {
             imagesView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             imagesView.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -4),
          
-            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
-            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 11),
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 9),
+            backButton.widthAnchor.constraint(equalToConstant: 24),
+            backButton.heightAnchor.constraint(equalToConstant: 24)
         ])
     }
     private lazy var backButton: UIButton = {
-          let button = UIButton(type: .system)
-          button.setTitle("Back", for: .normal)
-          button.setTitleColor(.systemBlue, for: .normal)
+        let button = UIButton(type: .custom)
+          button.setImage(UIImage(resource: .backward), for: .normal)
           button.translatesAutoresizingMaskIntoConstraints = false
-          
           button.addTarget(self, action: #selector(backToFirstScreen), for: .touchUpInside)
           return button
       }()
     @objc private func backToFirstScreen() {
            guard let navController = navigationController else { return }
-           
-           // Возвращаем исходный экран как root
-           let sourceVC = ImagesListViewController()
+           let sourceVC = MainTabBarController()
            navController.setViewControllers([sourceVC], animated: true)
        }
 }
